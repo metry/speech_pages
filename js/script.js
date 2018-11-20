85c5197a13f7bbc190b54bdd36ba1df6be1b8cf4
metry <fortrun.php@gmail.com>
2018-11-20 21:16:52 +0300

    js fix new

--- js/script.js
+++ js/script.js
@@ -1,54 +1,52 @@
-$( document ).ready(function() {
-	
-	function control(name) {
-		if (name == 'включить свет') {
-			
-		}
-		if (name == 'выключить свет') {
-			
-		}
-		if (name == 'опустить шторы') {
-			$(".image2").hide();
-		}
-		if (name == 'поднять шторы') {
-			$(".image2").show();
-		}
+function control(name) {
+	if (name == 'включить свет') {
+		
 	}
-	
-	var recognizer = new webkitSpeechRecognition();
-
-	// Ставим опцию, чтобы распознавание началось ещё до того, как пользователь закончит говорить
-	recognizer.interimResults = true;
-
-	// Какой язык будем распознавать?
-	recognizer.lang = 'ru-Ru';
-
-	// Используем колбек для обработки результатов
-	recognizer.onresult = function (event) {
-	var result = event.results[event.resultIndex];
-	if (result.isFinal) {
-	  console.log('Вы сказали: ' + result[0].transcript);
-	} else {
-	  console.log('Промежуточный результат: ', result[0].transcript);
+	if (name == 'выключить свет') {
+		
 	}
-	};
-
-	var synth = window.speechSynthesis;
-	var utterance = new SpeechSynthesisUtterance('How about we say this now? This is quite a long sentence to say.');
-
-	function stop () {
-		synth.pause();
+	if (name == 'опустить шторы') {
+		$(".image2").hide();
 	}
-	
-	function start () {
-		recognizer.start();
+	if (name == 'поднять шторы') {
+		$(".image2").show();
 	}
-	
-	
+}
+
+var recognizer = new webkitSpeechRecognition();
+
+// Ставим опцию, чтобы распознавание началось ещё до того, как пользователь закончит говорить
+recognizer.interimResults = true;
+
+// Какой язык будем распознавать?
+recognizer.lang = 'ru-Ru';
+
+// Используем колбек для обработки результатов
+recognizer.onresult = function (event) {
+var result = event.results[event.resultIndex];
+if (result.isFinal) {
+  console.log('Вы сказали: ' + result[0].transcript);
+} else {
+  console.log('Промежуточный результат: ', result[0].transcript);
+}
+};
+
+var synth = window.speechSynthesis;
+var utterance = new SpeechSynthesisUtterance('How about we say this now? This is quite a long sentence to say.');
+
+function stop () {
+	synth.pause();
+}
+
+function start () {
+	recognizer.start();
+}
+
+$( document ).ready(function() {	
     $("#start").on( "click", function() {
+		start();
 		$('html, body').animate({
         scrollTop: $("#image").offset().top
 		}, 2000);
-		start();
 	});
 });
