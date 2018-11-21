3f2da8542b6e60fdc3d3257910a047f3f75b6471
metry <fortrun.php@gmail.com>
2018-11-21 11:14:51 +0300

    add styles and results block

--- js/script.js
+++ js/script.js
@@ -15,40 +15,35 @@ function control(name) {
 }
 
 var recognizer = new webkitSpeechRecognition();
-
 // Ставим опцию, чтобы распознавание началось ещё до того, как пользователь закончит говорить
 recognizer.interimResults = true;
-
 // Какой язык будем распознавать?
 recognizer.lang = 'ru-Ru';
-
 // Используем колбек для обработки результатов
 recognizer.onresult = function (event) {
-var result = event.results[event.resultIndex];
-if (result.isFinal) {
-  console.log('Вы сказали: ' + result[0].transcript);
-  control(result[0].transcript);
-} else {
-  console.log('Промежуточный результат: ', result[0].transcript);
-}
+	var result = document.getElementById("results");
+	
+	var result = event.results[event.resultIndex];
+	if (result.isFinal) {
+	  console.log('Вы сказали: ' + result[0].transcript);
+	  result.innerHTML = result[0].transcript;
+	  control(result[0].transcript);
+	} else {
+	  console.log('Промежуточный результат: ', result[0].transcript);
+	  result.innerHTML = result[0].transcript + '...';
+	}
 };
-
-var synth = window.speechSynthesis;
-var utterance = new SpeechSynthesisUtterance('How about we say this now? This is quite a long sentence to say.');
-
-function stop () {
-	synth.pause();
-}
 
 function start () {
 	recognizer.start();
 }
 
 $( document ).ready(function() {	
-    $("#start").on( "click", function() {
+    $("#start, #startMenu").on( "click", function() {
 		$('html, body').animate({
         scrollTop: $("#image").offset().top
 		}, 2000);
+		return false;
 	});
 
 	$("#microphone").on( "click", function() {
